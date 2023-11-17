b4a619d9966902f7dbfd1f78dd9d802348224bab
chenle02 <chenle02@gmail.com>
2023-11-17 09:40:16 -0500

    Refine the document, add TOC.

--- documentation/Build.sh
+++ documentation/Build.sh
@@ -13,22 +13,29 @@ then
   exit 1
 fi
 
-sed '1s/\[11pt\]{article}/\[preview\]{standalone}/' ../Examples/FoxH32-21_output.tex           > FoxH32-21_output.tex
-sed '1s/\[11pt\]{article}/\[preview\]{standalone}/' ../Examples/FoxH32-21-Y_output.tex         > FoxH32-21-Y_output.tex
-sed '1s/\[11pt\]{article}/\[preview\]{standalone}/' ../Examples/FoxH32-21-Z-Star_output.tex    > FoxH32-21-Z-Star_output.tex
-sed '1s/\[11pt\]{article}/\[preview\]{standalone}/' ../Examples/FoxH32-21-Z_output.tex         > FoxH32-21-Z_output.tex
-sed '1s/\[11pt\]{article}/\[preview\]{standalone}/' ../Examples/FoxH-Cos_output.tex            > FoxH-Cos_output.tex
-sed '1s/\[11pt\]{article}/\[preview\]{standalone}/' ../Examples/FoxH-Mittag-Leffler_output.tex > FoxH-Mittag-Leffler_output.tex
-sed '1s/\[11pt\]{article}/\[preview\]{standalone}/' ../Examples/FoxH-Sin_output.tex            > FoxH-Sin_output.tex
 
-sed -i 's/{text}{/{text}{..\/Examples\//g' FoxH32-21_output.tex
-sed -i 's/{text}{/{text}{..\/Examples\//g' FoxH32-21_output.tex
-sed -i 's/{text}{/{text}{..\/Examples\//g' FoxH32-21-Y_output.tex
-sed -i 's/{text}{/{text}{..\/Examples\//g' FoxH32-21-Z-Star_output.tex
-sed -i 's/{text}{/{text}{..\/Examples\//g' FoxH32-21-Z_output.tex
-sed -i 's/{text}{/{text}{..\/Examples\//g' FoxH-Cos_output.tex
-sed -i 's/{text}{/{text}{..\/Examples\//g' FoxH-Mittag-Leffler_output.tex
-sed -i 's/{text}{/{text}{..\/Examples\//g' FoxH-Sin_output.tex
+# Array of filenames
+files=(
+    "FoxH32-21_output.tex"
+    "FoxH32-21-Y_output.tex"
+    "FoxH32-21-Z-Star_output.tex"
+    "FoxH32-21-Z_output.tex"
+    "FoxH-Cos_output.tex"
+    "FoxH-Mittag-Leffler_output.tex"
+    "FoxH-Sin_output.tex"
+)
+
+# Loop over each file
+for file in "${files[@]}"; do
+    # Apply the sed command to transform the first line
+    sed '1s/\[11pt\]{article}/\[preview\]{standalone}/' "../Examples/${file}" > "${file}"
+
+    # Apply the sed command to modify {text}{
+    sed -i 's/{text}{/{text}{..\/Examples\//g' "${file}"
+
+    # Change section to subsection
+    sed -i 's/section/subsection/g' "${file}"
+done
 
 lualatex --shell-escape --interaction=nonstopmode FoxH-Parametration
 bibtex FoxH-Parametration
---
+++ documentation/myzsh.zsh
@@ -0,0 +1,1 @@
+alias build='./Build.sh Yes'
