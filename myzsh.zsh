480da665c081be6a27f4275f9909a28718dba869
chenle02 <chenle02@gmail.com>
2023-11-26 00:23:28 -0500

    Update topics

--- myzsh.zsh
+++ myzsh.zsh
@@ -8,7 +8,16 @@ Screenshot() {
   import "./media/Screenshot_${filename}.png"
 }
 
-alias description="gh repo edit --description 'Some symbolic tools for the Fox H-function (V1.42)'"
+alias description="gh repo edit \
+  --description 'Some symbolic tools for the Fox H-function (V1.42)'\
+  --homepage 'https://doi.org/10.5281/zenodo.10149874' \
+  --add-topic 'research' \
+  --add-topic 'mathematics' \
+  --add-topic 'mathematica' \
+  --add-topic 'special-functions' \
+  --add-topic 'software' \
+  --add-topic 'cli' \
+  "
 
 
 # Build the documentation of the project
